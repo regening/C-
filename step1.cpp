ec391028c5ec2b915cb019bf679e1fcc23ed12d5
regening <2791186095@qq.com>
2026-01-20 22:58:15 +0800

    添加 step1.cpp 和 step1.exe

---
+++ step1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, C++!" << endl;
+    int number;
+    cin >> number;
+    cout << "You entered: " << number << endl;
+    return 0;
+}
