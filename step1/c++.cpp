14a55da7abcef4253ba2743c41a00dd8f7043a00
regening <2791186095@qq.com>
2026-01-22 00:00:25 +0800

    Step1

--- step1/c++.cpp
+++ step1/c++.cpp
@@ -1,6 +1,15 @@
 #include <iostream>                   // 引入标准输入输出库
 using namespace std;                  // 使用标准命名空间
 int main() {
-      cout << "Hello, World!" << endl;
-      return 0;
+    auto a = 5;                       // x 自动推导为 int
+    const int MAX_VALUE = 100;        // 与 C 一样  定义常量
+    int x = 10;
+    if (x > 5) {
+        cout << "x is greater than 5" << endl;
+    } else {
+        cout << "x is less than or equal to 5" << endl;
+    }
+
+    cout << "Hello, World!" << endl;
+    return 0;
 }
